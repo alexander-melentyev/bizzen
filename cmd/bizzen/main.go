d244d82b13bd3fe040ac8be6cbf778707b1e656a
Alexander Melentyev <alexander.melentyev@icloud.com>
2021-03-11 19:31:11 +0300

    feat: Added entrypoint

---
+++ cmd/bizzen/main.go
@@ -0,0 +1,38 @@
+package main //nolint:funlen
+
+import (
+	"os"
+
+	_orgHandler "github.com/alexander-melentyev/bizzen/internal/org/delivery/http"
+	_orgRepo "github.com/alexander-melentyev/bizzen/internal/org/repository"
+	_orgUseCase "github.com/alexander-melentyev/bizzen/internal/org/usecase"
+	"github.com/gin-gonic/gin"
+	_ "github.com/jackc/pgx/stdlib"
+	"github.com/jmoiron/sqlx"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+
+	conn, err := sqlx.Connect("pgx", os.Getenv("DSN"))
+	if err != nil {
+		log.Warn().Msg("can't connect to database: " + err.Error())
+	}
+
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.New()
+
+	prefix := r.Group("api")
+
+	v1 := prefix.Group("v1")
+
+	orgRepo := _orgRepo.NewRepository(conn)
+	orgUseCase := _orgUseCase.NewUseCase(orgRepo)
+	_orgHandler.NewHandler(v1, orgUseCase)
+
+	if err := r.Run(); err != nil {
+		log.Warn().Msg("Server start error: " + err.Error())
+	}
+}
